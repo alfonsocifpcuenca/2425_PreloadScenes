27d175ed3d1fa30fb13b56c522dbf423f754f82b
alfonsocifpcuenca <alfonso.rodriguez@cifpcuenca.es>
2025-06-12 17:08:13 +0200

    Se añade escena

--- Assets/Scripts/LoadScene.cs
+++ Assets/Scripts/LoadScene.cs
@@ -1,15 +1,11 @@
 using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UIElements;
 
 public class LoadScene : MonoBehaviour
 {
     [SerializeField] private GameObject loaderUI;
     [SerializeField] private string sceneToLoad;
-
-    private AsyncOperation loadOperation;
-    private float minimumLoadTime = 10f;
 
     public void OnMouseDown()
     {
@@ -43,6 +39,8 @@ public class LoadScene : MonoBehaviour
         if (loaderScript != null)
             loaderScript.SetPercentage(1f);
 
+        Debug.Log($"[CARGA] 100%");
+        Debug.Log($"[CARGA] Esparamos 1 segundo para entrar en la nueva escena");
         yield return new WaitForSeconds(1f);
 
         asyncLoad.allowSceneActivation = true;
--- Assets/Scripts/LoaderUI.cs
+++ Assets/Scripts/LoaderUI.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,15 +6,11 @@ public class LoaderUI : MonoBehaviour
     [Header("UI Elements")]
     public Slider loadingSlider;
     public Text loadingText;
-    public GameObject loadingScreen;
-
-    [Header("Scene Configuration")]
-    public string sceneToLoad;
 
     public void SetPercentage(float percentage)
     {
         this.loadingSlider.value = percentage;
         int percentageInt = Mathf.RoundToInt(percentage * 100);
-        this.loadingText.text = $"Cargando: {percentageInt}%";
+        this.loadingText.text = $"{percentageInt}%";
     }
 }
